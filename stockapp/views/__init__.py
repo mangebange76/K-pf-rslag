538786a0398ce9b7432b8ead118e275b047cf037
mangebange76 <magnusberg47@gmail.com>
2025-09-27 18:49:33 +0200

    Update __init__.py

--- stockapp/views/__init__.py
+++ stockapp/views/__init__.py
@@ -1,1 +1,14 @@
+# stockapp/views/__init__.py
+from .analysis import analysvy
+from .control import kontrollvy
+from .edit import lagg_till_eller_uppdatera
+from .portfolio import visa_portfolj
+from .proposals import visa_investeringsforslag
 
+__all__ = [
+    "analysvy",
+    "kontrollvy",
+    "lagg_till_eller_uppdatera",
+    "visa_portfolj",
+    "visa_investeringsforslag",
+]

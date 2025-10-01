3e5b191a600f611d6298f31cd2d439def7559e99
mangebange76 <magnusberg47@gmail.com>
2025-10-01 14:11:10 +0200

    Update __init__.py

--- stockapp/__init__.py
+++ stockapp/__init__.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Minimal package initializer för att undvika cirkulära/import-biverkningar.
-Importera alltid delmoduler direkt, t.ex.:
-    from stockapp.config import FINAL_COLS
-    from stockapp.fetchers.yahoo import fetch_all
-    from stockapp.scoring import sektorviktad_score
-osv.
+Minimal init för stockapp.
+
+VIKTIGT:
+- Importera INTE andra delmoduler här.
+- Låt appen/modulerna själva importera det de behöver.
+Detta undviker cirkulära importer (t.ex. när app.py importerar stockapp.utils).
 """
 
-from __future__ import annotations
-
-__all__ = []        # vi re-exporterar inget från rotpaketet
-__version__ = "0.1.0"
+__all__ = []  # håll tomt för att undvika oavsiktlig wildcard-export
